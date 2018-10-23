f078d040a6c1d68e3983c2019b8016eed8f624e9
lampjesse <lampjesse@163.com>
2018-10-23 16:47:07 +0800

    基于udp协议实现的黏包

---
+++ OOP/socket/nianbao/demo1/UDP/server.py
@@ -0,0 +1,16 @@
+from socket import *
+ip_port=('127.0.0.1',9000)
+bufsize=1024
+
+udp_client=socket(AF_INET,SOCK_DGRAM)
+
+
+while True:
+    msg=input('>>: ').strip()
+    udp_client.sendto(msg.encode('utf-8'),ip_port)
+    err,addr=udp_client.recvfrom(bufsize)
+    out,addr=udp_client.recvfrom(bufsize)
+    if err:
+        print('error : %s'%err.decode('utf-8'),end='')
+    if out:
+        print(out.decode('utf-8'), end='')
