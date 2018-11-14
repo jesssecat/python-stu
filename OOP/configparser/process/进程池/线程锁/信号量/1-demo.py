d90f3b5c92b37b630b93a6b92dbb9c26a2d243d2
lampjesse <lampjesse@163.com>
2018-11-14 17:16:43 +0800

    信号量保护

---
+++ OOP/configparser/process/进程池/线程锁/信号量/1-demo.py
@@ -0,0 +1,13 @@
+#保护线程的分次执行
+#可以设定每次执行几个
+import time
+from threading import Semaphore,Thread
+def func(sem,a,b):
+	sem.acquire()
+	time.sleep(1)
+	print(a+b)
+	sem.release()
+sem = Semaphore(4)
+for i in range(50):
+	t = Thread(target=func,args=(sem,i,i+5))
+	t.start()
